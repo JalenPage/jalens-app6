33b9c36496a60db4a4c57f12162b57c9eeb03fa4
Jalen Page <pagejale@msu.edu>
2015-02-01 17:34:03 -0500

    first change to app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,6 @@ require 'sinatra'
 class MyWebApp < Sinatra::Base
   get '/' do
     "Welcome to the webapp of DOOM, where the copyright is always up to date! Copyright 2014-#{ Time.now.year } Chris Fritz."
+    "Jalen Page"
   end
 end
